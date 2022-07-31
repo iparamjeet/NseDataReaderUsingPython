344b79d3cf466ab96d04a4e0f75056618e4a6b79
iParamjeet <paramjeetyadav94@gmail.com>
2022-07-31 13:36:27 +0530

    Paramjeet: added two more excel for defferent data

--- NseIndia.py
+++ NseIndia.py
@@ -5,25 +5,26 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
+
 class NSE:
-    pre_market_categories = ['NIFTY 50', 'Nifty Bank', 'Emerge', 'Securities in F&O', 'Others', 'ALL']
+    pre_market_categories = ['NIFTY 50', 'Nifty Bank',
+                             'Emerge', 'Securities in F&O', 'Others', 'ALL']
     equity_market_categories = [...]
     holidays_categories = ["clearing", "Trading"]
 
-
     def __init__(self):
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
         self.session = requests.Session()
-        self.session.get("http://nseindia.com", headers = self.headers)
-
+        self.session.get("http://nseindia.com", headers=self.headers)
 
     def pre_market_data(self, category):
-        pre_market_category= {"NIFTY 50": "NIFTY", "Nifty Bank": "BANKNIFTY", "Emerge": "SME", "Securities in F&O": "FO",
-                            "Others": "OTHERS", "ALL": "ALL"}
+        pre_market_category = {"NIFTY 50": "NIFTY", "Nifty Bank": "BANKNIFTY", "Emerge": "SME", "Securities in F&O": "FO",
+                               "Others": "OTHERS", "ALL": "ALL"}
         data = self.session.get(f"https://www.nseindia.com/api/market-data-pre-open?key={pre_market_category[category]}",
-                        headers = self.headers).json()["data"]
+                                headers=self.headers).json()["data"]
 
-        df = pd.DataFrame(data).to_excel("DataBook.xlsx")
+        df = pd.DataFrame(data).to_excel("Data_pre_Equitymarket.xlsx")
 
         new_data = []
         for i in data:
@@ -34,12 +35,12 @@ class NSE:
 
     def equity_market_data(self, category, symbol_list=False):
         category = category.upper().replace(' ', '%20').replace('&', '%26')
-        data= self.session.get(f"https://www.nseindia.com/api/equity-stockIndices?index={category}", headers =self.headers).json()["data"]
-       
-        print(data[0])
-        #pd.read_json("data.json").to_excel("output.xlsx")
-        df = pd.DataFrame(data).to_excel("DataBook.xlsx")
+        data = self.session.get(
+            f"https://www.nseindia.com/api/equity-stockIndices?index={category}", headers=self.headers).json()["data"]
 
+        print(data[0])
+        # pd.read_json("data.json").to_excel("output.xlsx")
+        df = pd.DataFrame(data).to_excel("Data_liveEquityMarket.xlsx")
 
         df = pd.DataFrame(data)
         df = df.drop(["meta"], axis=1)
@@ -50,42 +51,43 @@ class NSE:
             return df
 
     def about_holidays(self, category):
-        data= self.session.get(f'https://www.nseindia.com/api/holiday-master?type={category.lower()}', headers=self.headers).json() 
+        data = self.session.get(
+            f'https://www.nseindia.com/api/holiday-master?type={category.lower()}', headers=self.headers).json()
         df = pd.DataFrame(list(data.values())[0])
         return df
 
     def equity_info(self, symbol, trade_info=False):
-        symbol =symbol.replace('', '%20').replace('&', '%26')
-        url = 'https://www.nseindia.com/api/quote-equity?symbol=' + symbol + ("&section=trade_info" if trade_info else "") 
-        data= self.session.get(url, headers=self.headers).json()
+        symbol = symbol.replace('', '%20').replace('&', '%26')
+        url = 'https://www.nseindia.com/api/quote-equity?symbol=' + \
+            symbol + ("&section=trade_info" if trade_info else "")
+        data = self.session.get(url, headers=self.headers).json()
         return data
 
-
     def future_data(self, symbol, indices=False):
-        symbol =symbol.replace(' ', '%20').replace('&', '%26')
+        symbol = symbol.replace(' ', '%20').replace('&', '%26')
         url = 'https://www.nseindia.com/api/quote-derivative?symbol=' + symbol
         data = self.session.get(url, headers=self.headers).json()
         lst = []
         for i in data["stocks"]:
-            if i["metadata"]["instrumentType"] ==("Index Futures" if indices else "Stock Futures"): 
+            if i["metadata"]["instrumentType"] == ("Index Futures" if indices else "Stock Futures"):
                 lst.append(i["metadata"])
         df = pd.DataFrame(lst)
         df = df.set_index("identifier", drop=True)
         return df
 
-
-    def option_data (self, symbol, indices=False):
+    def option_data(self, symbol, indices=False):
         symbol = symbol.replace('', '%20').replace('&', '%26')
         if not indices:
             url = 'https://www.nseindia.com/api/option-chain-equities?symbole=' + symbol
         else:
             url = 'https://www.nseindia.com/api/option-chain-indices?symbol=' + symbol
-        data= self.session.get(url,headers=self.headers).json() ["records"]["data"]
+        data = self.session.get(url, headers=self.headers).json()[
+            "records"]["data"]
         my_df = []
         for i in data:
             for k, v in i.items():
-                if k == "CE" or k == "PE": 
-                    info = v 
+                if k == "CE" or k == "PE":
+                    info = v
                     info["instrumentType"] = k
                     my_df.append(info)
         df = pd.DataFrame(my_df)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ nse.equity_market_data("NIFTY 50")
 #print(len(nse.equity_market_data('Securities in F&O', symbol_list=True)))
 
 #nse.equity_market_data("NIFTY 50", symbol_list=True)
-#print(nse.holidays_categories)
+# print(nse.holidays_categories)
 
-#print(nse.pre_market_categories)
-#nse.pre_market_data("Nifty Bank")
+# print(nse.pre_market_categories)
+nse.pre_market_data("Nifty Bank")
